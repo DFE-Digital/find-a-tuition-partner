eea5c7ed665c7368e805fd3cbc0383af54e53c29
David Winchurch <david.winchurch@iterativeit.com>
2022-04-25 13:25:41 +0100

    File -> New Project Find a Tutoring Partner

---
+++ FindaTutoringPartner/FindaTutoringPartner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FindaTutoringPartner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
