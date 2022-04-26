b182001df1c1f77fa23139e36ed18278e8628876
David Winchurch <david.winchurch@iterativeit.com>
2022-04-26 06:52:41 +0100

    Basic clean architecture projects

---
+++ FindaTutoringPartner/Domain/ReferenceDataType.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain;
+
+public enum ReferenceDataType
+{
+    Address = 1,
+    Subject,
+    TutorType,
+    SessionType
+}
--- FindaTutoringPartner/FindaTutoringPartner/Handlers/SearchTutoringPartners/School.cs
+++ FindaTutoringPartner/FindaTutoringPartner/Handlers/SearchTutoringPartners/School.cs
@@ -1,14 +1,17 @@
-﻿namespace FindaTutoringPartner.Handlers.SearchTutoringPartners;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FindaTutoringPartner.Handlers.SearchTutoringPartners;
 
 public class School
 {
     public class Query
     {
-
+        public string? Postcode { get; set; }
     }
 
     public class Command
     {
-
+        [Required(ErrorMessage = "Enter a postcode")]
+        public string? Postcode { get; set; }
     }
 }
