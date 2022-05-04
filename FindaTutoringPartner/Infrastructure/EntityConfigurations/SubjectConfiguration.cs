48163f326ea2b72c3149bff37ae0f09a46643eda
David Winchurch <david.winchurch@iterativeit.com>
2022-05-04 16:18:08 +0100

    Seed data

--- FindaTutoringPartner/Infrastructure/EntityConfigurations/SubjectConfiguration.cs
+++ FindaTutoringPartner/Infrastructure/EntityConfigurations/SubjectConfiguration.cs
@@ -8,5 +8,15 @@ public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
 {
     public void Configure(EntityTypeBuilder<Subject> builder)
     {
+        builder.HasData(
+            new Subject {Id = 1, Name = "Primary - Literacy"},
+            new Subject {Id = 2, Name = "Primary - Numeracy"},
+            new Subject {Id = 3, Name = "Primary - Science"},
+            new Subject {Id = 4, Name = "Secondary - English"},
+            new Subject {Id = 5, Name = "Secondary - Humanities"},
+            new Subject {Id = 6, Name = "Secondary - Maths"},
+            new Subject {Id = 7, Name = "Secondary - Modern Foreign Languages"},
+            new Subject {Id = 8, Name = "Secondary - Science"}
+            );
     }
 }
--- FindaTutoringPartner/Infrastructure/EntityConfigurations/TutorTypeConfiguration.cs
+++ FindaTutoringPartner/Infrastructure/EntityConfigurations/TutorTypeConfiguration.cs
@@ -8,5 +8,14 @@ public class TutorTypeConfiguration : IEntityTypeConfiguration<TutorType>
 {
     public void Configure(EntityTypeBuilder<TutorType> builder)
     {
+        builder.HasData(
+            new TutorType { Id = 1, Name = "Qualified Teachers" },
+            new TutorType { Id = 2, Name = "Professional Tutors" },
+            new TutorType { Id = 3, Name = "SEN Specialists" },
+            new TutorType { Id = 4, Name = "Higher Level Teaching Assistants" },
+            new TutorType { Id = 5, Name = "University Students" },
+            new TutorType { Id = 6, Name = "Volunteer tutors" },
+            new TutorType { Id = 7, Name = "No preference" }
+        );
     }
 }
