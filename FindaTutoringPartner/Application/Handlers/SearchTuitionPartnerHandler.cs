b6f3b5e34038b32976f43b0636ba1f107f89268b
David Winchurch <david.winchurch@iterativeit.com>
2022-05-13 12:17:53 +0100

    NTP-55: Return all TPs from API call

--- FindaTutoringPartner/Application/Handlers/SearchTuitionPartnerHandler.cs
+++ FindaTutoringPartner/Application/Handlers/SearchTuitionPartnerHandler.cs
@@ -2,6 +2,7 @@
 using Domain.Search;
 using Domain.Validators;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Handlers;
 
@@ -17,8 +18,32 @@ public class SearchTuitionPartnerHandler
 
     }
 
-    public async Task<SearchResultsPage<TuitionPartnerSearchRequest, TuitionPartner>> Handle(Command request, CancellationToken cancellationToken)
+    public class Handler : IRequestHandler<Command, SearchResultsPage<TuitionPartnerSearchRequest, TuitionPartner>>
     {
-        throw new NotImplementedException();
+        private readonly INtpDbContext _dbContext;
+
+        public Handler(INtpDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<SearchResultsPage<TuitionPartnerSearchRequest, TuitionPartner>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var queryable = _dbContext.TuitionPartners.AsQueryable();
+
+            switch (request.OrderBy)
+            {
+                case TuitionPartnerOrderBy.Name:
+                    queryable = request.Direction == OrderByDirection.Descending
+                        ? queryable.OrderByDescending(e => e.Name)
+                        : queryable.OrderBy(e => e.Name);
+                    break;
+            }
+
+            var count = await queryable.CountAsync(cancellationToken);
+            var results = await queryable.Skip(request.Page * request.PageSize).Take(request.PageSize).ToArrayAsync(cancellationToken);
+
+            return new SearchResultsPage<TuitionPartnerSearchRequest, TuitionPartner>(request, count, results);
+        }
     }
 }
--- FindaTutoringPartner/Domain/Search/TuitionPartnerSearchRequest.cs
+++ FindaTutoringPartner/Domain/Search/TuitionPartnerSearchRequest.cs
@@ -4,4 +4,6 @@ public class TuitionPartnerSearchRequest : SearchRequestBase
 {
     public decimal Latitude { get; set; }
     public decimal Longitude { get; set; }
+    public TuitionPartnerOrderBy OrderBy { get; set; } = TuitionPartnerOrderBy.Name;
+    public OrderByDirection Direction { get; set; } = OrderByDirection.Ascending;
 }
