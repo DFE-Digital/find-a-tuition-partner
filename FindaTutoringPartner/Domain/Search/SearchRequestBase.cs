6edcb269e27d937fcae06f35a32d43280d8fbf1e
David Winchurch <david.winchurch@iterativeit.com>
2022-05-13 13:46:07 +0100

    Correct default values for swagger

--- FindaTutoringPartner/Domain/Search/SearchRequestBase.cs
+++ FindaTutoringPartner/Domain/Search/SearchRequestBase.cs
@@ -1,9 +1,12 @@
-﻿namespace Domain.Search;
+﻿using System.ComponentModel;
+
+namespace Domain.Search;
 
 public abstract class SearchRequestBase
 {
     public const int DefaultPageSize = 50;
     
     public int Page { get; set; }
+    [DefaultValue(DefaultPageSize)]
     public int PageSize { get; set; } = DefaultPageSize;
 }
