eb5b57635760b6f1ad6ad45cb42172369941d4d3
David Winchurch <david.winchurch@iterativeit.com>
2022-05-17 16:21:00 +0100

    NTP-57: Interface to map postcode to location filter parameters

---
+++ FindaTutoringPartner/Domain/Search/LocationFilterParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Search;
+
+public class LocationFilterParameters
+{
+    public string Postcode { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
+    public string Country { get; set; }
+    public string? Region { get; set; }
+    public string LocalAuthorityCode { get; set; }
+    public string LocalAuthority { get; set; }
+}
