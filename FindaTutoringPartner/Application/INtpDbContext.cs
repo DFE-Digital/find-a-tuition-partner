fb2a02964e0d3ae349abdb2c6ee15e6e2b110875
Vreddy1 <vipin.reddy@education.gov.uk>
2022-05-20 14:14:54 +0100

    added order to the interface

--- FindaTutoringPartner/Application/INtpDbContext.cs
+++ FindaTutoringPartner/Application/INtpDbContext.cs
@@ -6,13 +6,14 @@ namespace Application;
 public interface INtpDbContext
 {
     DbSet<Address> Addresses { get; set; }
-    DbSet<Subject> Subjects { get; set; }
+    DbSet<LocalAuthority> LocalAuthorities { get; set; }
     DbSet<Region> Regions { get; set; }
+    DbSet<Subject> Subjects { get; set; }
     DbSet<TuitionPartner> TuitionPartners { get; set; }
     DbSet<TuitionPartnerLocation> TuitionPartnerLocations { get; set; }
     DbSet<TutorType> TutorTypes { get; set; }
     DbSet<UserSearch> UserSearches { get; set; }
     DbSet<UserSession> UserSessions { get; set; }
-    DbSet<LocalAuthority> LocalAuthorities { get; set; }
+   
     Task<int> SaveChangesAsync();
 }
