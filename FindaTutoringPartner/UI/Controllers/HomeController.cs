b40d43a5029aaafceafbc9462731a92c51f1d6d7
David Winchurch <david.winchurch@iterativeit.com>
2022-06-10 10:19:11 +0100

    NTP-252: Compliance pages from footer links

--- FindaTutoringPartner/UI/Controllers/HomeController.cs
+++ FindaTutoringPartner/UI/Controllers/HomeController.cs
@@ -13,11 +13,6 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
