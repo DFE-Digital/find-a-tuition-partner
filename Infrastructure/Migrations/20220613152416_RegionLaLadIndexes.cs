f33661b84459dabf3c5ab655726422fed8e4e913
David Winchurch <david.winchurch@iterativeit.com>
2022-06-13 16:24:42 +0100

    NTP-250: Indexes

---
+++ Infrastructure/Migrations/20220613152416_RegionLaLadIndexes.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class RegionLaLadIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Regions_Name",
+                table: "Regions",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocalAuthorityDistricts_Name",
+                table: "LocalAuthorityDistricts",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocalAuthority_Name",
+                table: "LocalAuthority",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Regions_Name",
+                table: "Regions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LocalAuthorityDistricts_Name",
+                table: "LocalAuthorityDistricts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LocalAuthority_Name",
+                table: "LocalAuthority");
+        }
+    }
+}
