c45eb4fc4311e4e116c138864c7cd189a799ed66
David Winchurch <david.winchurch@iterativeit.com>
2022-06-14 22:46:27 +0100

    NTP-296: Changed the default page width to match designs

--- UI/wwwroot/webpack.assets.config.js
+++ UI/wwwroot/webpack.assets.config.js
@@ -5,7 +5,8 @@ module.exports = {
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: path.join(__dirname, 'node_modules/govuk-frontend/govuk/assets'), to: path.join(__dirname, 'assets') }
+        { from: path.join(__dirname, 'node_modules/govuk-frontend/govuk/assets'), to: path.join(__dirname, 'assets') },
+        { from: path.join(__dirname, 'node_modules/govuk-frontend/govuk/all.js'), to: path.join(__dirname, 'dist/govuk.js') }
       ],
     }),
   ],
