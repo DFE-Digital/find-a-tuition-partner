06d2e7262433ba79e83a1fd77e378e8259020494
Ben Arnold <ben@arnoldfamily.me.uk>
2022-06-23 00:32:10 +0100

    Use fixture ctor

--- Tests/SearchForATutor.cs
+++ Tests/SearchForATutor.cs
@@ -6,11 +6,13 @@ namespace Tests;
 [Collection(nameof(SliceFixture))]
 public class SearchForATutor
 {
+    private readonly SliceFixture fixture;
+
+    public SearchForATutor(SliceFixture fixture) => this.fixture = fixture;
+
     [Fact]
     public async void Starting_a_search_creates_a_search_record()
     {
-        var fixture = new SliceFixture();
-
         var search = await fixture.SendAsync(new Search.Command { });
 
         var savedSearch = await fixture.GetDbAsync<UserSearch>();
