03aa7947e84f7cd8d590744fe6a00d9ef7651720
David Winchurch <david.winchurch@iterativeit.com>
2022-06-28 11:48:40 +0100

    NTP-365: TuitionPartnerValidator

---
+++ Domain.Tests/Validators/TuitionPartnerValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Validators;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Domain.Tests.Validators;
+
+public class TuitionPartnerValidatorTests
+{
+    private readonly TuitionPartnerValidator _validator;
+
+    public TuitionPartnerValidatorTests()
+    {
+        _validator = new TuitionPartnerValidator();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void With_no_name(string name)
+    {
+        var model = new TuitionPartner { Name = name };
+        var result = _validator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(null)]
+    public void With_no_description(string description)
+    {
+        var model = new TuitionPartner { Description = description };
+        var result = _validator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.Description);
+    }
+}
---
+++ Domain/Validators/TuitionPartnerValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Domain.Validators;
+
+public class TuitionPartnerValidator : AbstractValidator<TuitionPartner>
+{
+    public TuitionPartnerValidator()
+    {
+        RuleFor(m => m.Name)
+            .NotEmpty()
+            .WithMessage("Enter a name");
+
+        RuleFor(m => m.Description)
+            .NotEmpty()
+            .WithMessage("Enter a description");
+    }
+}
