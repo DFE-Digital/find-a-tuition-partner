9fc44a2aa41c37ba2065446ad840ac4204fdc1af
David Winchurch <david.winchurch@iterativeit.com>
2022-07-01 14:15:01 +0100

    Basic back link JavaScript

---
+++ UI/src/javascript/back-link.js
@@ -0,0 +1,18 @@
+function BackLink() {
+    this.$links = document.querySelectorAll('.govuk-back-link')
+}
+
+BackLink.prototype.init = function () {
+    if (this.$links) {
+        this.$links.forEach(element => {
+            element.addEventListener('click', this.linkClickedEvent.bind(this))
+        });
+    }
+}
+
+BackLink.prototype.linkClickedEvent = function (e) {
+    e.preventDefault();
+    history.back();
+}
+
+export default BackLink
