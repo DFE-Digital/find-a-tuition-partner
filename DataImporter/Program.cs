f6ff3df8687f70e1c1cfb92369491388bbe9e670
David Winchurch <david.winchurch@iterativeit.com>
2022-07-06 13:05:19 +0100

    NTP-383: Provide option to generate encryption key

--- DataImporter/Program.cs
+++ DataImporter/Program.cs
@@ -1,10 +1,20 @@
-﻿using Infrastructure.Extensions;
+﻿using System.Security.Cryptography;
+using Infrastructure.Extensions;
 using Microsoft.Extensions.Hosting;
 using Infrastructure;
 using Infrastructure.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-if (args.Length > 0 && args.Any(x => x == "encrypt"))
+if (args.Any(x => x == "generate-key"))
+{
+    using var crypto = Aes.Create();
+    var base64Key = Convert.ToBase64String(crypto.Key);
+    Console.WriteLine($"Generated data file encryption key {base64Key}");
+    Console.WriteLine($"use encrypt --DataEncryption:SourceDirectory \"<DIRECTORY>\" --DataEncryption:Key {base64Key} to encrypt data files with this key");
+    return;
+}
+
+if (args.Any(x => x == "encrypt"))
 {
     var host = Host.CreateDefaultBuilder(args)
         .ConfigureServices((hostContext, services) =>
