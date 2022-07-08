ff4be1742e451490368fa1b8dd91891c0cad693e
David Winchurch <david.winchurch@iterativeit.com>
2022-07-08 12:59:44 +0100

    NTP-377: BDD tests

--- UI/cypress/e2e/homepage.js
+++ UI/cypress/e2e/homepage.js
@@ -8,10 +8,6 @@ When("a user visits the homepage", () => {
   Step(this, "a user has arrived on the homepage");
 });
 
-Then("the heading should say {string}", (heading) => {
-  cy.get("h1").should("have.text", heading);
-});
-
 When("they click 'Start now'", () => {
   cy.get('[data-testid="call-to-action"]').click();
 });
--- UI/cypress/support/step_definitions/pages.js
+++ UI/cypress/support/step_definitions/pages.js
@@ -15,3 +15,11 @@ Then("they will be taken to the 'Find a tuition partner' journey start page", ()
 Then("they will be taken to the 'Which key stages' page", () => {
     cy.location('pathname').should('eq', '/find-a-tuition-partner/which-key-stages');
 });
+
+Then("the page URL ends with {string}", url => {
+    cy.location('pathname').should('match', new RegExp(`${url}$`));
+});
+
+Then("the heading should say {string}", (heading) => {
+    cy.get("h1").should("have.text", heading);
+});
