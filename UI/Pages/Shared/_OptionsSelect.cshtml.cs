71b8e8490ab46f23c2d9e59c30e03d567c4c1b54
Ben Arnold <ben@arnoldfamily.me.uk>
2022-07-14 13:51:17 +0100

    NTP-355: Options select chevron points down when the options are collapsed

--- UI/Pages/Shared/_OptionsSelect.cshtml.cs
+++ UI/Pages/Shared/_OptionsSelect.cshtml.cs
@@ -2,5 +2,5 @@
 
 public record OptionsSelectModel(string Name, string DisplayName, IEnumerable<(string Name, string Value, string DisplayName, bool Selected)> Items)
 {
-    public string ClosedClass => Items.Any(x => x.Selected) ? "" : "js-closed";
+    public string ClosedData => Items.Any(x => x.Selected) ? "" : @"data-closed-on-load=true";
 }
