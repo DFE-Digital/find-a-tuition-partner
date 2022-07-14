65e15a930f47bd91f004b2062a02397e74857ec3
Ben Arnold <ben@arnoldfamily.me.uk>
2022-07-14 09:06:34 +0100

    NTP-355: Collapse unselected key stages when search results page loads

--- UI/cypress/e2e/results.js
+++ UI/cypress/e2e/results.js
@@ -23,8 +23,10 @@ Then("they will see all the subjects for {string}", keystage => {
 
         const subjects = allSubjects[element]
 
-        cy.get(`[data-testid=${kebabCase(keystage)}]`)
-        .parent().parent()
+        cy.get(`[data-testid=${kebabCase(element)}]`)
+            .should('not.have.class', 'js-closed')
+
+        cy.get(`[data-testid=${kebabCase(element)}]`)
         .within(() =>
         {
             cy.get('[data-testid="subject-name"]').each((item, index) => {
@@ -35,21 +37,13 @@ Then("they will see all the subjects for {string}", keystage => {
     });
 });
 
-Then("they will see a collapsed subject filter for {string}}", keystage => {
+Then("they will see a collapsed subject filter for {string}", keystage => {
     const stages = keystage.split(',').map(s => s.trim());
     
     stages.forEach(element => {
 
-        const subjects = allSubjects[element]
-
-        cy.get(`[data-testid=${kebabCase(keystage)}]`)
-        .parent().parent()
-        .within(() =>
-        {
-            cy.get('[data-testid="subject-name"]').each((item, index) => {
-                cy.wrap(item).should('contain.text', subjects[index])
-            });
-        });
+        cy.get(`[data-testid=${kebabCase(element)}]`).
+            should('have.class', 'js-closed')
 
     });
 });
