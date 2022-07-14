43f3cb2f1e2100a8007017668563e336686fe076
Ben Arnold <ben@arnoldfamily.me.uk>
2022-07-14 13:33:15 +0100

    NTP-355: Create partial view for collapsible Options Select

---
+++ UI/Pages/Shared/_OptionsSelect.cshtml.cs
@@ -0,0 +1,6 @@
+﻿namespace UI.Pages.Shared;
+
+public record OptionsSelectModel(string Name, string DisplayName, IEnumerable<(string Name, string Value, string DisplayName, bool Selected)> Items)
+{
+    public string ClosedClass => Items.Any(x => x.Selected) ? "" : "js-closed";
+}
