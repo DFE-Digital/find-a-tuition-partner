c88b6d571e40a97d948c66c7f78b3ac0957eeac8
David Winchurch <david.winchurch@iterativeit.com>
2022-07-18 12:34:57 +0100

    NTP-411: Get query string based on schools' phase of education

--- GiasPostcodeSearch/GiasPostcodeSearchService.cs
+++ GiasPostcodeSearch/GiasPostcodeSearchService.cs
@@ -27,20 +27,39 @@ public class GiasPostcodeSearchService : IHostedService
         var totalElapsedMilliseconds = 0L;
         var minElapsedMilliseconds = long.MaxValue;
         var maxElapsedMilliseconds = 0L;
+        string fastestRequestUri = "";
+        string slowestRequestUri = "";
+
+        var runStopWatch = new Stopwatch();
+        runStopWatch.Start();
 
         foreach (var schoolDatum in schoolData)
         {
             _logger.LogDebug($"Searching for Tuition Partners covering School {schoolDatum}");
 
-            var requestUri = $"search-results?Postcode={schoolDatum.Postcode}";
+            var subjectsQueryString = GetSubjectsQueryString(schoolDatum);
+            if (subjectsQueryString == null)
+            {
+                _logger.LogInformation($"School {schoolDatum} is not valid for this service");
+            }
+
+            var requestUri = $"search-results?Postcode={schoolDatum.Postcode}&{subjectsQueryString}";
             
             var stopWatch = new Stopwatch();
             stopWatch.Start();
             var response = await _httpClient.GetAsync(requestUri, cancellationToken);
             stopWatch.Stop();
             totalElapsedMilliseconds += stopWatch.ElapsedMilliseconds;
-            minElapsedMilliseconds = Math.Min(minElapsedMilliseconds, stopWatch.ElapsedMilliseconds);
-            maxElapsedMilliseconds = Math.Max(minElapsedMilliseconds, stopWatch.ElapsedMilliseconds);
+            if (stopWatch.ElapsedMilliseconds < minElapsedMilliseconds)
+            {
+                minElapsedMilliseconds = stopWatch.ElapsedMilliseconds;
+                fastestRequestUri = requestUri;
+            }
+            if (stopWatch.ElapsedMilliseconds > maxElapsedMilliseconds)
+            {
+                maxElapsedMilliseconds = stopWatch.ElapsedMilliseconds;
+                slowestRequestUri = requestUri;
+            }
 
             if (response.IsSuccessStatusCode)
             {
@@ -56,8 +75,9 @@ public class GiasPostcodeSearchService : IHostedService
             if (count == 10) break;
         }
 
-        var averageElapsedMilliseconds = (int)(totalElapsedMilliseconds / (double)count);
-        _logger.LogInformation($"{count} searches run. Average response time {averageElapsedMilliseconds}ms min {minElapsedMilliseconds}ms max {maxElapsedMilliseconds}ms");
+        runStopWatch.Stop();
+        var averageElapsedMilliseconds = (int)(runStopWatch.ElapsedMilliseconds / (double)count);
+        _logger.LogInformation($"{count} searches run in {runStopWatch.ElapsedMilliseconds/1000}s. Average response time {averageElapsedMilliseconds}ms min {minElapsedMilliseconds}ms ({fastestRequestUri}) max {maxElapsedMilliseconds}ms ({slowestRequestUri})");
 
         _host.StopApplication();
     }
@@ -65,5 +85,21 @@ public class GiasPostcodeSearchService : IHostedService
     public Task StopAsync(CancellationToken cancellationToken)
     {
         return Task.CompletedTask;
+    }
+
+    private string? GetSubjectsQueryString(SchoolDatum schoolDatum)
+    {
+        switch (schoolDatum.PhaseOfEducation)
+        {
+            case PhaseOfEducation.Primary:
+            case PhaseOfEducation.MiddleDeemedPrimary:
+                return "Data.Subjects=KeyStage1-English&Data.Subjects=KeyStage2-Maths&Data.Subjects=KeyStage2-Science";
+            case PhaseOfEducation.Secondary:
+            case PhaseOfEducation.MiddleDeemedSecondary:
+                return "Data.Subjects=KeyStage3-English&Data.Subjects=KeyStage3-Maths&Data.Subjects=KeyStage4-Science";
+            case PhaseOfEducation.AllThrough:
+                return "Data.Subjects=KeyStage1-English&Data.Subjects=KeyStage1-Maths&Data.Subjects=KeyStage1-Science&Data.Subjects=KeyStage2-English&Data.Subjects=KeyStage2-Maths&Data.Subjects=KeyStage2-Science&Data.Subjects=KeyStage3-English&Data.Subjects=KeyStage3-Maths&Data.Subjects=KeyStage3-Science&Data.Subjects=KeyStage4-English&Data.Subjects=KeyStage4-Maths&Data.Subjects=KeyStage4-Science";
+            default: return null;
+        }
     }
 }
