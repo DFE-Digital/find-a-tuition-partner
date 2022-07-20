b856f730b1780ecbacab11cf7411a5a2b0bbee4a
Stephen McNeill <stephenjmcneill@yahoo.co.uk>
2022-07-20 12:28:06 +0100

    NTP-315: Added end to end testing for external website links, drop down widgets and added edited layout.

--- UI/cypress/e2e/tuition-partner-details.js
+++ UI/cypress/e2e/tuition-partner-details.js
@@ -8,9 +8,13 @@ Given("a user has arrived on the 'Tuition Partner' page for {string} after enter
     cy.visit(`/search-results?Data.Subjects=KeyStage1-English&Data.TuitionType=Any&Data.Postcode=sk11eb`);
     cy.get('.govuk-link').contains(name).click();
 });
-
+  
 When("the home page is selected", () => {
     cy.get('[data-testid="home-link"]').click();
+});
+
+When("they click 'What is a quality assured tuition partner?'", () => {
+    cy.get('[data-testid="qatp-details"]').click();
 });
 
 Then("TP has not provided the information in the {string} section", details => {
@@ -28,6 +32,26 @@ Then("the search details are correct", () => {
     cy.location('search').should('eq', '?Postcode=sk11eb&TuitionType=Any&Subjects=KeyStage1-English');
 });
 
+Then("the quality assured tuition partner details are hidden", () => {
+    cy.get('[data-testid="qatp-details"]').should("not.have.attr", "open");
+});
 
+Then("the payment details are hidden", () => {
+    cy.get('[data-testid="payment-details"]').should("not.have.attr", "open");
+});
 
+Then("the quality assured tuition partner details are shown", () => {
+    cy.get('[data-testid="qatp-details"]').should("have.attr", "open");
+  });
+
+Then("the tuition partners website link exist", () => {
+    cy.get('[data-testid=tuition-partner-website-link]').should('have.prop', 'href');
+});
+
+Then("the funding guidance page is accessible", () => {
+    cy.get('[data-testid=funding-guidance]').then(function ($a) {
+        const href = $a.prop('href');
+        cy.request(href).its('body').should('include', '</html>');
+    })
+});
 
