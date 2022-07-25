4b33e20a51749a0f6537e64cc8bd59633b054809
Ben Arnold <ben@arnoldfamily.me.uk>
2022-07-25 15:39:15 +0100

    Fix duplicate step error in e2e tests

--- UI/cypress/support/step_definitions/results-page.js
+++ UI/cypress/support/step_definitions/results-page.js
@@ -5,10 +5,6 @@ Given("a user has arrived on the 'Search results' page for {string}", keyStageSu
     cy.visit(`/search-results?postcode=sk11eb&${KeyStageSubjects('subjects', keyStageSubject)}`);
 });
 
-Given("a user has arrived on the 'Search results' page for 'Key stage 1'", keyStage => {
-    cy.visit(`/search-results?postcode=AB12CD&key-subjects=KeyStage1&subjects=KeyStage1-English`);
-});
-
 Given("a user has arrived on the 'Search results' page for {string} without a postcode", keyStage => {
     cy.visit(`/search-results?key-subjects=KeyStage1&subjects=KeyStage1-English`);
 });
