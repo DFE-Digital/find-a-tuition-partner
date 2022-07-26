a137ecaa2a2b11e0faf6acd79acee8f17e655643
Stephen McNeill <stephenjmcneill@yahoo.co.uk>
2022-07-26 12:18:14 +0100

    NTP-406: Added changes so that the cookis banner no longer relies on javascript. It now uses the cs class of cookies.cshtml

---
+++ UI/Pages/Cookies.cshtml.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Pages
+{
+    public class Cookies : PageModel
+    {
+		private const string ConsentCookieName = ".FindATuitionPartner.Consent";
+		public IActionResult OnGet(bool? consent, string returnUrl)
+		{
+			if (consent.HasValue)
+			{
+				var cookieOptions = new CookieOptions { Expires = DateTime.Today.AddMonths(12), Secure = true };
+				Response.Cookies.Append(ConsentCookieName, consent.Value.ToString(), cookieOptions);
+				return new RedirectResult(returnUrl);
+			}
+			return Page();
+		}
+	}
+}
