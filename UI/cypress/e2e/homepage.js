3d291da79af571d3af0db3b75c1d0c2235a91ca8
Vreddy1 <vipin.reddy@education.gov.uk>
2022-07-26 14:28:36 +0100

    NTP-416:Updated content css and e2e feature files

---
+++ UI/cypress/e2e/homepage.js
@@ -0,0 +1,18 @@
+﻿import { Given, When, Then, Step } from "@badeball/cypress-cucumber-preprocessor";
+
+
+Then("check other tutoring options text is displayed", () => {
+    cy.get('[data-testid="other-tutoring-options"]').should('contain.text', 'Other tutoring options')
+});
+
+
+Then("the other options academic links to {string}", (href) => {
+    cy.get('[data-testid="academic-mentors-link"]')
+        .should('have.attr', 'href', href)
+});
+
+
+Then("the other options school-led tutoring links to {string}", (href) => {
+    cy.get('[data-testid="school-led-tutoring-link"]')
+        .should('have.attr', 'href', href)
+});
