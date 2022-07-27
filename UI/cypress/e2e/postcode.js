1bcfde41c143659a97db81291b98aa3ae54ac167
Vreddy1 <vipin.reddy@education.gov.uk>
2022-07-27 09:33:03 +0100

    NTP-416:Removed the feature file and moved the tests to postcode feature file

--- UI/cypress/e2e/postcode.js
+++ UI/cypress/e2e/postcode.js
@@ -16,3 +16,19 @@ Then("the quality assured tuition partner details are hidden", () => {
 Then("the quality assured tuition partner details are shown", () => {
   cy.get('[data-testid="qatp-details"]').should("have.attr", "open");
 });
+
+Then("check other tutoring options text is displayed", () => {
+    cy.get('[data-testid="other-tutoring-options"]').should('contain.text', 'Other tutoring options')
+});
+
+
+Then("the other options academic links to {string}", (href) => {
+    cy.get('[data-testid="academic-mentors-link"]')
+        .should('have.attr', 'href', href)
+});
+
+
+Then("the other options school-led tutoring links to {string}", (href) => {
+    cy.get('[data-testid="school-led-tutoring-link"]')
+        .should('have.attr', 'href', href)
+});
