3cb2dfe0050ef2d552e664c5a0912b54ccd80eb0
Vreddy1 <vipin.reddy@education.gov.uk>
2022-07-27 09:36:34 +0100

    NTP-416: removed unwanted space

--- UI/cypress/e2e/postcode.js
+++ UI/cypress/e2e/postcode.js
@@ -21,12 +21,10 @@ Then("check other tutoring options text is displayed", () => {
     cy.get('[data-testid="other-tutoring-options"]').should('contain.text', 'Other tutoring options')
 });
 
-
 Then("the other options academic links to {string}", (href) => {
     cy.get('[data-testid="academic-mentors-link"]')
         .should('have.attr', 'href', href)
 });
-
 
 Then("the other options school-led tutoring links to {string}", (href) => {
     cy.get('[data-testid="school-led-tutoring-link"]')
