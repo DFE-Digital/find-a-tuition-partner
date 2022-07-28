ea43b1d08e6ce06bf0e67ff9dd7cf8d5043fde73
Vreddy1 <vipin.reddy@education.gov.uk>
2022-07-28 15:45:17 +0100

    NTP-387:accessibility statement added for accessibility  page

---
+++ UI/cypress/e2e/accessibility.feature
@@ -0,0 +1,36 @@
+﻿Feature: Accessibility page shows users how accessible the website and how to report problems
+ Scenario: page as header accessibility statement
+    Given a user has arrived on the accessibility page
+    Then they will see the Accessibility statement header
+
+ Scenario: page as header accessibility statement
+    Given a user has arrived on the accessibility page
+    Then they will see the Accessibility statement header
+
+ Scenario: page as link to design priniciples
+    Given a user has arrived on the accessibility page
+    Then they will see the design priniciples link
+
+ Scenario: page as header for cannot access part
+    Given a user has arrived on the accessibility page
+    Then they will see cannot access part header
+
+ Scenario: page as link to mail ntp service
+    Given a user has arrived on the accessibility page
+    Then they will see link to ntp mail address
+
+ Scenario: page as header for reporting accesibility problems
+    Given a user has arrived on the accessibility page
+    Then they will see reporting accesibility problems header
+
+ Scenario: page as header for enforcement procedure header
+    Given a user has arrived on the accessibility page
+    Then they will see enforcement procedure header
+
+ Scenario: page as link to equality advisory service
+    Given a user has arrived on the accessibility page
+    Then they will see link to equality advisory service
+
+ Scenario: page as header improve accesibility
+    Given a user has arrived on the accessibility page
+    Then they will see header improve accesibility
---
+++ UI/cypress/e2e/accessibility.js
@@ -0,0 +1,37 @@
+﻿import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("a user has arrived on the accessibility page", () => {
+    cy.visit(`/accessibility`);
+});
+
+Then("they will see the Accessibility statement header", () => {
+    cy.get('[data-testid="accessibility-header"]').should('contain.text', "Accessibility statement")
+});
+
+Then("they will see the design priniciples link", () => {
+    cy.get('[data-testid="design-principles-link"]').should('have.attr', 'href', 'https://design-system.service.gov.uk/accessibility/')
+});
+
+Then("they will see cannot access part header", () => {
+    cy.get('[data-testid="access-parts-header"]').should('contain.text', "What to do if you cannot access parts of this website")
+});
+
+Then("they will see link to ntp mail address", () => {
+    cy.get('[data-testid="mailto-ntp-link"]').should('have.attr', 'href', 'mailto:ntp.digital@education.gov.uk')
+});
+
+Then("they will see reporting accesibility problems header", () => {
+    cy.get('[data-testid="accessibility-reporting-header"]').should('contain.text', "Reporting accessibility problems with this website")
+});
+
+Then("they will see enforcement procedure header", () => {
+    cy.get('[data-testid="accessibility-enforcement-header"]').should('contain.text', "Enforcement procedure")
+});
+
+Then("they will see link to equality advisory service", () => {
+    cy.get('[data-testid="contact-quality-link"]').should('have.attr', 'href', 'https://www.equalityadvisoryservice.com/')
+});
+
+Then("they will see header improve accesibility", () => {
+    cy.get('[data-testid="improve-accessibility-header"]').should('contain.text', "What we’re doing to improve accessibility")
+});
