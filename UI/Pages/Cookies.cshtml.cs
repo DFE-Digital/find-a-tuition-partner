787d1865863e57eacfd724c8ab2ba8f299c7f369
Stephen McNeill <stephenjmcneill@yahoo.co.uk>
2022-08-10 12:46:42 +0100

    NTP-479: Removed space in using and changes order for format error

--- UI/Pages/Cookies.cshtml.cs
+++ UI/Pages/Cookies.cshtml.cs
@@ -1,7 +1,7 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.ComponentModel.DataAnnotations;
+
 
 namespace UI.Pages
 {
