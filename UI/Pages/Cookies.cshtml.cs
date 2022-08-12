a44365a426206ffbf66dcfec6f5c6b63bc4a7f01
David Winchurch <david.winchurch@digital.education.gov.uk>
2022-08-12 15:58:08 +0100

    NTP-516: Default all GA cookie related consent to denied (#96)

--- UI/Pages/Cookies.cshtml.cs
+++ UI/Pages/Cookies.cshtml.cs
@@ -3,69 +3,71 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 
-namespace UI.Pages
+namespace UI.Pages;
+
+public class Cookies : PageModel
 {
-    public class Cookies : PageModel
+    public const string ConsentCookieName = ".FindATuitionPartner.Consent";
+
+    [BindProperty]
+    [Required(ErrorMessage = "You must select an option")]
+    public bool? Consent { get; set; }
+    [BindProperty(SupportsGet = true)]
+    public string? ReturnUrl { get; set; }
+    public bool PreferencesSet { get; set; }
+
+    public IActionResult OnGet(bool? consent, bool? preferencesSet)
     {
-        private const string ConsentCookieName = ".FindATuitionPartner.Consent";
-
-        [BindProperty]
-        [Required(ErrorMessage = "You must select an option")]
-        public bool? Consent { get; set; }
-        public bool PreferencesSet { get; set; } = false;
-
-        [BindProperty(SupportsGet = true)]
-        public string? ReturnUrl { get; set; }
-
-        public IActionResult OnGet(bool? consent)
+        Consent = consent;
+        if (preferencesSet.HasValue)
         {
-            Consent = consent;
-
-            if (Request.Cookies.ContainsKey(ConsentCookieName))
-            {
-                var value = Request.Cookies[ConsentCookieName];
-                if (value != null)
-                {
-                    Consent = bool.Parse(value);
-                }
-            }
-
-            if (consent.HasValue)
-            {
-                ApplyCookieConsent(consent);
-                if (!string.IsNullOrEmpty(ReturnUrl))
-                {
-                    return Redirect(ReturnUrl);
-                }
-            }
-            return Page();
+            PreferencesSet = preferencesSet.Value;
         }
 
-        public IActionResult OnPost()
+        if (Request.Cookies.ContainsKey(ConsentCookieName))
         {
-            if (!ModelState.IsValid) return Page();
-
-            PreferencesSet = true;
-            ApplyCookieConsent(Consent);
-
-            return Page();
+            var value = Request.Cookies[ConsentCookieName];
+            if (value != null)
+            {
+                Consent = bool.Parse(value);
+            }
         }
 
-        private void ApplyCookieConsent(bool? consent)
+        if (consent.HasValue)
         {
-            if (consent.HasValue)
+            ApplyCookieConsent(consent);
+            if (!string.IsNullOrEmpty(ReturnUrl))
             {
-                var cookieOptions = new CookieOptions { Expires = DateTime.Today.AddMonths(12), Secure = true };
-                Response.Cookies.Append(ConsentCookieName, consent.Value.ToString(), cookieOptions);
+                return Redirect(ReturnUrl);
+            }
+        }
 
-                if (!consent.Value)
+        return Page();
+    }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid) return Page();
+
+        ApplyCookieConsent(Consent);
+
+        return RedirectToPage(new { returnUrl = ReturnUrl, preferencesSet = true });
+    }
+
+    private void ApplyCookieConsent(bool? consent)
+    {
+        if (consent.HasValue)
+        {
+            var cookieOptions = new CookieOptions { Expires = DateTime.Today.AddMonths(12), Secure = true };
+            Response.Cookies.Append(ConsentCookieName, consent.Value.ToString(), cookieOptions);
+
+            if (!consent.Value)
+            {
+                foreach (var cookie in Request.Cookies.Keys)
                 {
-                    foreach (var cookie in Request.Cookies.Keys)
+                    if (cookie.StartsWith("_ga") || cookie.Equals("_gid"))
                     {
-                        if (cookie.StartsWith("_ga") || cookie.Equals("_gid"))
-                        {
-                            Response.Cookies.Delete(cookie);
-                        }
+                        Response.Cookies.Delete(cookie);
                     }
                 }
             }
