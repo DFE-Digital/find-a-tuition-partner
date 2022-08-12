c91e06906f9570e657eb3b1581f19a25bf98f510
Ben Arnold <ben@arnoldfamily.me.uk>
2022-08-12 22:23:14 +0100

    Destructure search object and tuition partner results

--- Infrastructure/MetricLogging/SearchTuitionPartnerHandlerLoggingBehaviour.cs
+++ Infrastructure/MetricLogging/SearchTuitionPartnerHandlerLoggingBehaviour.cs
@@ -20,7 +20,7 @@ public class SearchTuitionPartnerHandlerLoggingBehaviour
         CancellationToken cancellationToken,
         RequestHandlerDelegate<TuitionPartnerSearchResultsPage> next)
     {
-        using var scope = _logger.BeginScope("SearchRequest", request);
+        using var _ = _logger.BeginScope("{@Search}", request);
         _logger.LogInformation("Searching for tuition partners");
 
         var stopwatch = new Stopwatch();
@@ -30,10 +30,11 @@ public class SearchTuitionPartnerHandlerLoggingBehaviour
 
         var names = response.Results.Select(x => x.Name).ToList();
         var logLevel = response.Count == 0 ? LogLevel.Warning : LogLevel.Information;
-        using (_logger.BeginScope("TuitionPartners", names))
+
+        using (_logger.BeginScope("{@TuitionPartners}", names))
         {
             _logger.Log(logLevel, "Found {Count} results in {time}ms",
-                response.Count, stopwatch.ElapsedMilliseconds);
+                    response.Count, stopwatch.ElapsedMilliseconds);
         }
 
         return response;
