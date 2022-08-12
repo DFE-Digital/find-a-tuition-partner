1ee8b17633af76fa06d66e7e5c30a8fe6fd23bde
David Winchurch <david.winchurch@digital.education.gov.uk>
2022-08-12 16:24:24 +0100

    NTP-513: HTML titles (#98)
    * NTP-513: Update academic mentors title with e2e tests
    
    * NTP-513: Update school led tutoring title plus e2e tests
    
    * NTP-513: Accessibility statement title plus e2e tests
    
    * NTP-513: Update cookies title plus e2e tests
    
    * NTP-513: Update funding and privacy page titles plus e2e tests
    
    * NTP-513: Fix end to end tests following merge

--- UI/cypress/e2e/schoolledtutoring.js
+++ UI/cypress/e2e/schoolledtutoring.js
@@ -1,10 +1,10 @@
 ﻿import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
 
-Given("a user has arrived on the school led page", () => {
+Given("a user has arrived on the school led tutoring page", () => {
     cy.visit(`/school-led-tutoring`);
 });
 
-Then("they will see the school led header", () => {
+Then("they will see the school led tutoring header", () => {
     cy.get('[data-testid="school-led-header"]').should('contain.text', "Employ your own tutor")
 });
 
