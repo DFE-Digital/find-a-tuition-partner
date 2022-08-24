06fb2c22bfaf0e2625aed3876cd6335dd4eff387
Vreddy1 <vipin.reddy@education.gov.uk>
2022-08-24 14:02:04 +0100

    NTP-540: fixed the broken e2e tests by removing the page and adding back

---
+++ UI/Pages/ContactUs.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Pages
+{
+    public class ContactUsModel : PageModel
+    {
+        public string? ReturnPath { get; set; }
+
+        public IActionResult OnGet()
+        {
+            ReturnPath = Request.Headers["Referer"].ToString();
+
+            return Page();
+        }
+
+        public IActionResult OnPost(string returnUrl)
+        {
+            ReturnPath = Request.Headers["Referer"].ToString();
+
+            return Page();
+        }
+    }
+}
