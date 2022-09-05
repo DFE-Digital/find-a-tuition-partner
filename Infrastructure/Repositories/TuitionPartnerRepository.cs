481903788751794a75f7892c85d8c83baaa7081d
stephenjmcneill <33860585+stephenjmcneill@users.noreply.github.com>
2022-09-05 13:19:18 +0100

    NTP-567: type of tuition is blank when no postcode is specified (#137)
    * NTP-567: Added fix for missing tuition types and end to end tests
    
    * NTP-567: Corrected fix to avoid warning: DistinctAfterOrderByWithoutRowLimitingOperatorWarning
    
    * NTP-567: Formatted file
    
    * NTP-567: Avoid EF warning about distinct after ordering
    
    "The query uses the 'Distinct' operator after applying an ordering.  If there are any row limiting operation used before 'Distinct' and after ordering the ordering will be used for it.  Ordering(s) will be erased after 'Distinct' and results afterwards would be unordered'
    
    Co-authored-by: Ben Arnold <ben@arnoldfamily.me.uk>

--- Infrastructure/Repositories/TuitionPartnerRepository.cs
+++ Infrastructure/Repositories/TuitionPartnerRepository.cs
@@ -2,6 +2,7 @@
 using Domain.Search;
 using Mapster;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace Infrastructure.Repositories;
 
@@ -21,25 +22,35 @@ public class TuitionPartnerRepository : ITuitionPartnerRepository
         CancellationToken cancellationToken = default)
     {
         var entities = await _dbContext.TuitionPartners.AsNoTracking()
-            .Include(e => e.LocalAuthorityDistrictCoverage.Where(lad => lad.LocalAuthorityDistrictId == localAuthorityDistrictId))
+            .IncludeTuitionForLocalDistrict(localAuthorityDistrictId)
             .ThenInclude(e => e.TuitionType)
             .Include(e => e.SubjectCoverage)
             .ThenInclude(e => e.Subject)
             .Where(e => ids.Distinct().Contains(e.Id))
+            .AsSplitQuery()
             .ToListAsync(cancellationToken);
 
         var results = new List<TuitionPartnerSearchResult>(entities.Count);
         foreach (var entity in entities)
         {
             var result = entity.Adapt<TuitionPartnerSearchResult>();
-
             result.Subjects = entity.SubjectCoverage.OrderBy(e => e.Id).Select(e => e.Subject).Distinct().ToArray();
 
             result.TuitionTypes = entity.LocalAuthorityDistrictCoverage.Select(e => e.TuitionType).OrderByDescending(e => e.Id).Distinct().ToArray();
 
             results.Add(result);
         }
-
         return ordering.Order(results);
+    }
+}
+
+public static class LocalAuthorityDistrictCoverageQueryExtension
+{
+    public static IIncludableQueryable<Domain.TuitionPartner, IEnumerable<Domain.LocalAuthorityDistrictCoverage>>
+    IncludeTuitionForLocalDistrict(this IQueryable<Domain.TuitionPartner> tuitionPartners, int? localAuthorityDistrictId)
+    {
+        return localAuthorityDistrictId == null
+            ? tuitionPartners.Include(e => e.LocalAuthorityDistrictCoverage)
+            : tuitionPartners.Include(e => e.LocalAuthorityDistrictCoverage.Where(lad => lad.LocalAuthorityDistrictId == localAuthorityDistrictId));
     }
 }
