b8b89f26084abd773efe5dbe728394d1f9759f40
Stephen McNeill <stephenjmcneill@yahoo.co.uk>
2022-09-07 15:31:14 +0100

    NTP-486: Added complaints page

---
+++ UI/Pages/ComplaintsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UI.Pages
+{
+    public class ComplaintsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
