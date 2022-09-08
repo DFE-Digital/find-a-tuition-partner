fb344303ed97669631afee3ca9a9ed24a422a013
Stephen McNeill <stephenjmcneill@yahoo.co.uk>
2022-09-08 09:30:44 +0100

    NTP-486: Added updates to page, removed uneeded character, corrected end to end tests.

--- UI/cypress/e2e/complaints-page.feature
+++ UI/cypress/e2e/complaints-page.feature
@@ -3,18 +3,19 @@ Feature: Complaints page
   Scenario: Complaints page is displayed
     Given the ‘contact us’ page is displayed 
     When the link ‘read our guidance’ is selected
-    Then the page's title is 'Complaints Page'
+    Then they will be taken to the 'complaints' page
+    And the page's title is 'Complaints Page'
 
-  Scenario: page has link to ftp mail 
+  Scenario: page has link to tutoring support email address
     Given the 'complaints page' is displayed
-    Then they will see link to tutoring mail address
+    Then page has link to tutoring support email address
 
   Scenario: The home link is selected
     Given the 'complaints page' is displayed
     When the home link is selected
-    Then the page's title is 'Find a tuition partner'
+    Then they will be taken to the 'Find a tuition partner' journey start page
+    And the page's title is 'Find a tuition partner'
 
-  Scenario: Local council link is selected
+  Scenario: Local council link is accessible
     Given the 'complaints page' is displayed
-    When the ‘local council’ link has been selected
-    Then the 'report child abuse to local council' page is accessible
+    Then the 'report child abuse to local council' page is accessible in a new tab
--- UI/cypress/e2e/complaints-page.js
+++ UI/cypress/e2e/complaints-page.js
@@ -12,15 +12,11 @@ When("the home link is selected", () => {
   cy.get('[data-testid="home-link"]').click();
 });
 
-When("the ‘local council’ link has been selected", () => {
-  cy.get('[data-testid="local-council-link"]').click();
-});
-
 Then("the 'complaints page' is displayed", () => {
   cy.visit("/complaints-page");
 });
 
-Then("they will see link to tutoring mail address", () => {
+Then("page has link to tutoring support email address", () => {
   cy.get('[data-testid="mailto-ntp-link"]').should(
     "have.attr",
     "href",
@@ -28,16 +24,30 @@ Then("they will see link to tutoring mail address", () => {
   );
 });
 
-Then("the 'report child abuse to local council' page is accessible", () => {
-  cy.get('[data-testid="local-council-link"]').should(
-    "have.attr",
-    "href",
-    "https://www.gov.uk/report-child-abuse-to-local-council"
-  );
+Then(
+  "the 'report child abuse to local council' page is accessible in a new tab",
+  () => {
+    cy.get('[data-testid="local-council-link"]').should(
+      "have.attr",
+      "href",
+      "https://www.gov.uk/report-child-abuse-to-local-council"
+    );
 
-  cy.request("https://www.gov.uk/report-child-abuse-to-local-council").should(
-    (response) => {
-      expect(response.status).to.eq(200);
-    }
-  );
+    cy.get('[data-testid="local-council-link"]').should(
+      "have.attr",
+      "target",
+      "_blank"
+    );
+
+    cy.get('[data-testid="local-council-link"]').then(function ($a) {
+      const href = $a.prop("href");
+      cy.request(href).should((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+  }
+);
+
+Then("they will be taken to the 'complaints' page", () => {
+  cy.location("pathname").should("eq", "/complaints-page");
 });
