ac715376c06697a68fc6e36d181b285825b99268
Stephen McNeill <stephenjmcneill@yahoo.co.uk>
2022-09-20 15:23:54 +0100

    NTP-568: Added unit tests for setting, retrieving and deleting cookies

--- Tests/CookiesTests.cs
+++ Tests/CookiesTests.cs
@@ -1,15 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
 using Moq;
 using Cookies = UI.Pages.Cookies;
 
+
+
 namespace Tests;
-public class CookiesTests
+
+[Collection(nameof(SliceFixture))]
+public class CookiesTests : CleanSliceFixture
 {
+    public CookiesTests(SliceFixture fixture) : base(fixture)
+    {
+    }
 
     [Theory]
     [InlineData(true, true)]
@@ -31,19 +42,150 @@ public class CookiesTests
     }
 
     [Fact]
-    public void Cookies_OnPost_Model_Invalid_Test()
+    public async void Cookies_For_FindATuitionPartner_Created_OnGet()
     {
-        Cookies cookiePage = new();
-        cookiePage.PageContext.ModelState.AddModelError("Message.Text", "test error");
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.OnGet(true, true);
+                  return Task.FromResult(page);
+              });
+        cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
+        cookieVariables.ToString().Should().Contain(".FindATuitionPartner.Consent=True");
+    }
+
+    [Fact]
+    public async void Cookies_For_FindATuitionPartner_Retrieved_OnGet()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "true");
+                  page.OnGet(true, true);
+                  return Task.FromResult(page);
+              });
+        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner").Should().BeTrue();
+        cookiePage.Request.Cookies[".FindATuitionPartner"]?.ToString().Should().Be("true");
+        cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
+        cookieVariables.ToString().Should().Contain(".FindATuitionPartner.Consent=True");
+    }
+
+    [Fact]
+    public async void Cookies_For_FindATuitionPartner_Retrieved_And_Set_To_True_OnGet()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "false");
+                  page.OnGet(true, true);
+                  return Task.FromResult(page);
+              });
+        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner").Should().BeTrue();
+        cookiePage.Request.Cookies[".FindATuitionPartner"]?.ToString().Should().Be("false");
+
+        cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
+        cookieVariables.ToString().Should().Contain(".FindATuitionPartner.Consent=True");
+    }
+
+    [Fact]
+    public async void Cookies_For_FindATuitionPartner_Retrieved_And_Set_To_False_OnGet()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "true");
+                  page.OnGet(false, true);
+                  return Task.FromResult(page);
+              });
+        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner").Should().BeTrue();
+        cookiePage.Request.Cookies[".FindATuitionPartner"]?.ToString().Should().Be("true");
+
+        cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
+        cookieVariables.ToString().Should().Contain(".FindATuitionPartner.Consent=False");
+    }
+
+    [Fact]
+    public async void Cookies_Associated_With_FindATuitionPartner_Deleted_When_Set_To_False()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "true");
+                  page.Request.Cookies.ContainsKey("_ga").Should().BeTrue();
+                  page.Request.Cookies.ContainsKey("_gid").Should().BeTrue();
+                  var cookieOptions = new CookieOptions { Expires = DateTime.Today.AddMonths(12), HttpOnly = true, Secure = true, SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict };
+                  page.Response.Cookies.Append("_ga", "hasValueToDelete", cookieOptions);
+                  page.Response.Cookies.Append("_gid", "hasValueToDelete", cookieOptions);
+                  page.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
+                  cookieVariables.ToString().Should().Contain("_ga=hasValueToDelete;");
+                  cookieVariables.ToString().Should().Contain("_gid=hasValueToDelete;");
+                  page.OnGet(false, true);
+                  return Task.FromResult(page);
+              });
+        cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
+        cookieVariables.ToString().Should().Contain("_ga=;");
+        cookieVariables.ToString().Should().Contain("_gid=;");
+    }
+
+    [Fact]
+    public async void Cookies_OnPost_Model_Valid()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+            .Execute(page =>
+            {
+                page.PageContext = GetPageContext();
+                page.OnPost();
+                return Task.FromResult(page);
+            });
+        cookiePage.Should().BeOfType<Cookies>();
+        cookiePage.ModelState.IsValid.Should().BeTrue();
+
+        var result = cookiePage.OnPost();
+        result.Should().BeOfType<RedirectToPageResult>();
+    }
+
+    [Fact]
+    public async void Cookies_OnPost_Model_Invalid()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+           .Execute(page =>
+           {
+               page.PageContext = GetPageContext();
+               page.ModelState.AddModelError("cookies", "You must select an option");
+               page.OnPost();
+               return Task.FromResult(page);
+           });
+        cookiePage.Should().BeOfType<Cookies>();
+        cookiePage.ModelState.IsValid.Should().BeFalse();
+
+        cookiePage.ModelState.AddModelError("cookies", "You must select an option");
         var result = cookiePage.OnPost();
         result.Should().BeOfType<PageResult>();
     }
 
-    [Fact]
-    public void Cookies_OnPost_Model_Valid_Test()
+    private static IRequestCookieCollection MockRequestCookieCollection(string key, string value)
     {
-        Cookies cookiePage = new();
-        var result = cookiePage.OnPost();
-        result.Should().BeOfType<RedirectToPageResult>();
+        var requestFeature = new HttpRequestFeature();
+        var featureCollection = new FeatureCollection();
+        requestFeature.Headers = new HeaderDictionary();
+        requestFeature.Headers.Add(HeaderNames.Cookie, new StringValues(key + "=" + value));
+        requestFeature.Headers.Append(HeaderNames.Cookie, new StringValues("_ga" + "=" + "true"));
+        requestFeature.Headers.Append(HeaderNames.Cookie, new StringValues("_gid" + "=" + "true"));
+        featureCollection.Set<IHttpRequestFeature>(requestFeature);
+        var cookiesFeature = new RequestCookiesFeature(featureCollection);
+        return cookiesFeature.Cookies;
+    }
+
+    private static PageContext GetPageContext()
+    {
+        var httpContext = new DefaultHttpContext();
+        var modelState = new ModelStateDictionary();
+        var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+        return new PageContext(actionContext);
     }
 }
