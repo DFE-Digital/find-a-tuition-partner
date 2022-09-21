28c2b12c7d39018293b972ed29150350f9cbd786
Stephen McNeill <stephenjmcneill@yahoo.co.uk>
2022-09-21 16:07:20 +0100

    NTP-568: Added tests for returnUrl, PreferenceSet and corrected current tests.

--- Tests/CookiesTests.cs
+++ Tests/CookiesTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Http.Headers;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
@@ -11,8 +10,6 @@ using Microsoft.Net.Http.Headers;
 using Moq;
 using Cookies = UI.Pages.Cookies;
 
-
-
 namespace Tests;
 
 [Collection(nameof(SliceFixture))]
@@ -20,25 +17,6 @@ public class CookiesTests : CleanSliceFixture
 {
     public CookiesTests(SliceFixture fixture) : base(fixture)
     {
-    }
-
-    [Theory]
-    [InlineData(true, true)]
-    [InlineData(null, true)]
-    [InlineData(true, null)]
-    [InlineData(null, null)]
-    public void Cookies_OnGet_Test(bool? consent, bool? preferencesSet)
-    {
-        var httpContext = new DefaultHttpContext();
-        var modelState = new ModelStateDictionary();
-        var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
-        var pageContext = new PageContext(actionContext);
-        Cookies cookiePage = new()
-        {
-            PageContext = pageContext
-        };
-        var result = cookiePage.OnGet(consent, preferencesSet);
-        result.Should().BeOfType<PageResult>();
     }
 
     [Fact]
@@ -62,14 +40,13 @@ public class CookiesTests : CleanSliceFixture
               .Execute(page =>
               {
                   page.PageContext = GetPageContext();
-                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "true");
-                  page.OnGet(true, true);
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner.Consent", "true");
+                  page.OnGet(null, true);
                   return Task.FromResult(page);
               });
-        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner").Should().BeTrue();
-        cookiePage.Request.Cookies[".FindATuitionPartner"]?.ToString().Should().Be("true");
-        cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
-        cookieVariables.ToString().Should().Contain(".FindATuitionPartner.Consent=True");
+        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner.Consent").Should().BeTrue();
+        cookiePage.Request.Cookies[".FindATuitionPartner.Consent"]?.ToString().Should().Be("true");
+        cookiePage.Consent.Should().BeTrue();   
     }
 
     [Fact]
@@ -79,13 +56,12 @@ public class CookiesTests : CleanSliceFixture
               .Execute(page =>
               {
                   page.PageContext = GetPageContext();
-                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "false");
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner.Consent", "false");
                   page.OnGet(true, true);
                   return Task.FromResult(page);
               });
-        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner").Should().BeTrue();
-        cookiePage.Request.Cookies[".FindATuitionPartner"]?.ToString().Should().Be("false");
-
+        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner.Consent").Should().BeTrue();
+        cookiePage.Request.Cookies[".FindATuitionPartner.Consent"]?.ToString().Should().Be("false");
         cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
         cookieVariables.ToString().Should().Contain(".FindATuitionPartner.Consent=True");
     }
@@ -97,13 +73,12 @@ public class CookiesTests : CleanSliceFixture
               .Execute(page =>
               {
                   page.PageContext = GetPageContext();
-                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "true");
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner.Consent", "true");
                   page.OnGet(false, true);
                   return Task.FromResult(page);
               });
-        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner").Should().BeTrue();
-        cookiePage.Request.Cookies[".FindATuitionPartner"]?.ToString().Should().Be("true");
-
+        cookiePage.Request.Cookies.ContainsKey(".FindATuitionPartner.Consent").Should().BeTrue();
+        cookiePage.Request.Cookies[".FindATuitionPartner.Consent"]?.ToString().Should().Be("true");
         cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
         cookieVariables.ToString().Should().Contain(".FindATuitionPartner.Consent=False");
     }
@@ -115,21 +90,85 @@ public class CookiesTests : CleanSliceFixture
               .Execute(page =>
               {
                   page.PageContext = GetPageContext();
-                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner", "true");
-                  page.Request.Cookies.ContainsKey("_ga").Should().BeTrue();
-                  page.Request.Cookies.ContainsKey("_gid").Should().BeTrue();
+                  page.Request.Cookies = MockRequestCookieCollection(".FindATuitionPartner.Consent", "true");
                   var cookieOptions = new CookieOptions { Expires = DateTime.Today.AddMonths(12), HttpOnly = true, Secure = true, SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Strict };
                   page.Response.Cookies.Append("_ga", "hasValueToDelete", cookieOptions);
                   page.Response.Cookies.Append("_gid", "hasValueToDelete", cookieOptions);
                   page.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
-                  cookieVariables.ToString().Should().Contain("_ga=hasValueToDelete;");
-                  cookieVariables.ToString().Should().Contain("_gid=hasValueToDelete;");
                   page.OnGet(false, true);
                   return Task.FromResult(page);
               });
         cookiePage.Response.Headers.TryGetValue("Set-Cookie", out var cookieVariables);
         cookieVariables.ToString().Should().Contain("_ga=;");
         cookieVariables.ToString().Should().Contain("_gid=;");
+    }
+
+    [Fact]
+    public async void Cookies_Preference_Set_True()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.OnGet(false, true);
+                  return Task.FromResult(page);
+              });
+        cookiePage.PreferencesSet.Should().BeTrue();
+    }
+
+    [Fact]
+    public async void Cookies_Preference_Set_False()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.OnGet(false, false);
+                  return Task.FromResult(page);
+              });
+        cookiePage.PreferencesSet.Should().BeFalse();
+    }
+
+    [Fact]
+    public async void Cookies_Preference_Set_Null()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.OnGet(false, null);
+                  return Task.FromResult(page);
+              });
+        cookiePage.PreferencesSet.Should().BeFalse();
+    }
+
+    [Fact]
+    public async void Cookies_Preference_Set_Null_Return_Url()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.Request.Headers.Add("Referer", "https://localhost:7036/");
+                  page.OnGet(false, null);
+                  return Task.FromResult(page);
+              });
+        cookiePage?.ReturnUrl?.ToString().Should().Be("https://localhost:7036/");
+    }
+
+    [Fact]
+    public async void Cookies_Preference_Set_To_Value_Return_Url()
+    {
+        var cookiePage = await Fixture.GetPage<Cookies>()
+              .Execute(page =>
+              {
+                  page.PageContext = GetPageContext();
+                  page.TempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+                  page.TempData["ReturnUrl"] = "https://localhost:7036/";
+                  page.OnGet(false, true);
+                  return Task.FromResult(page);
+              });
+        cookiePage?.ReturnUrl?.ToString().Should().Be("https://localhost:7036/");
     }
 
     [Fact]
