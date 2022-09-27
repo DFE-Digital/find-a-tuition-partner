6f52fc530bbfbdac9b917f8107481e99a95d8aad
David Winchurch <david.winchurch@iterativeit.com>
2022-09-27 12:43:05 +0100

    NTP-605: Implement full list page according to ACs

--- UI/Pages/FullList.cshtml.cs
+++ UI/Pages/FullList.cshtml.cs
@@ -1,11 +1,23 @@
+using Application.Handlers;
+using Domain.Search;
+using MediatR;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace UI.Pages
+namespace UI.Pages;
+
+public class FullList : PageModel
 {
-    public class FullList : PageModel
+    private readonly IMediator _mediator;
+
+    public FullList(IMediator mediator) => _mediator = mediator;
+
+    public TuitionPartnerSearchResultsPage? Results { get; private set; }
+
+    public async Task OnGet()
     {
-        public void OnGet()
+        Results = await _mediator.Send(new SearchTuitionPartnerHandler.Command
         {
-        }
+            OrderBy = TuitionPartnerOrderBy.Name
+        });
     }
 }
