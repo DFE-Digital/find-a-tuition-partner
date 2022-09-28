47559cb13f1ea2b7cd44379f38cd900277818b5f
David Winchurch <david.winchurch@iterativeit.com>
2022-09-28 14:44:42 +0100

    NTP-607: Unit tests

--- Tests/ShowAllTuitionPartners.cs
+++ Tests/ShowAllTuitionPartners.cs
@@ -1,4 +1,8 @@
 ﻿using Domain.Constants;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using UI.Extensions;
 using UI.Pages;
 
 namespace Tests;
@@ -56,5 +60,82 @@ public class ShowAllTuitionPartners : CleanSliceFixture
             new { Name = "Beta" },
             new { Name = "Gamma" }
         }, options => options.WithStrictOrdering());
+    }
+
+    [Fact]
+    public async Task Search_by_name()
+    {
+        // Given
+        await Fixture.ExecuteDbContextAsync(async db =>
+        {
+            db.TuitionPartners.Add(new Domain.TuitionPartner
+            {
+                Name = "Alpha",
+                SeoUrl = "alpha",
+                Website = "http://"
+            });
+
+            db.TuitionPartners.Add(new Domain.TuitionPartner
+            {
+                Name = "Beta",
+                SeoUrl = "beta",
+                Website = "http://"
+            });
+
+            await db.SaveChangesAsync();
+        });
+
+        // When
+        var page = await Fixture.GetPage<FullList>()
+            .Execute(async page =>
+            {
+                page.Data.Name = "LPh";
+                await page.OnGet();
+                return page;
+            });
+
+        // Then
+        page.Results.Should().NotBeNull();
+        page.Results!.Results.Should().BeEquivalentTo(new[]
+        {
+            new { Name = "Alpha" }
+        });
+    }
+
+    [Fact]
+    public async Task Sets_from_full_list()
+    {
+        var page = await Fixture.GetPage<FullList>()
+            .Execute(async page =>
+            {
+                page.Data.From = ReferrerList.SearchResults;
+                await page.OnGet();
+                return page;
+            });
+
+        page.Data.From.Should().Be(ReferrerList.FullList);
+    }
+
+    [Fact]
+    public async Task Sets_AllSearchData()
+    {
+        var page = await Fixture.GetPage<FullList>()
+            .Execute(async page =>
+            {
+                page.TempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>());
+                page.Data.From = ReferrerList.SearchResults;
+                page.Data.Name = "test";
+                await page.OnGet();
+                return page;
+            });
+
+        var data = page.TempData.Peek<SearchModel>("AllSearchData");
+        data.Should().NotBeNull();
+        data!.From.Should().Be(ReferrerList.FullList);
+        data.Name.Should().Be("test");
+        data.Postcode.Should().BeNull();
+        data.Subjects.Should().BeNull();
+        data.TuitionType.Should().BeNull();
+        data.KeyStages.Should().BeNull();
     }
 }
