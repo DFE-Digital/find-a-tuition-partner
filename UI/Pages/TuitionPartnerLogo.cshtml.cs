71209dff92b3b16ea03aaaa906e5906b67237639
Ben Arnold <ben@arnoldfamily.me.uk>
2022-09-28 10:06:48 +0100

    NTP-556: Page to retrieve logo for tuition partner

---
+++ UI/Pages/TuitionPartnerLogo.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Application;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace UI.Pages;
+
+public class TuitionPartnerLogo : PageModel
+{
+    private readonly INtpDbContext ntpDb;
+
+    public TuitionPartnerLogo(INtpDbContext ntpDb) => this.ntpDb = ntpDb;
+
+    public async Task<FileContentResult> OnGet(string id)
+    {
+        var logo = await ntpDb.TuitionPartners
+            .Where(x => x.SeoUrl == id)
+            .Select(x => x.Logo)
+            .FirstOrDefaultAsync()
+            ?? throw new ArgumentException("No logo found", nameof(id));
+
+        var bytes = Convert.FromBase64String(logo);
+        return new FileContentResult(bytes, "image/svg+xml");
+    }
+}
