57da8af32bb96aa06779f174842b8b430e313af7
Ben Arnold <ben@arnoldfamily.me.uk>
2022-10-06 11:50:27 +0100

    NTP-630: Ensure logos are matched to the exact tuition partner name

---
+++ Tests/Importing/ImportLogoFiles.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure;
+
+namespace Tests.Importing;
+
+public class ImportLogoFiles
+{
+    [Theory]
+    [InlineData("seo-name", "Logo_seo-name.svg")]
+    public void Logo_file_matches_seo_name_exactly(string name, string logoFile)
+    {
+        DataImporterService.IsFileLogoForTuitionPartner(name, logoFile).Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("seo-name", "Logo_banana.svg")]
+    [InlineData("seo-name", "Logo_other-seo-name.svg")]
+    [InlineData("seo-name", "Logo_seo-name_logo.svg")]
+    [InlineData("seo-name", "seo-name.svg")]
+    public void Logo_file_does_not_match_seo_name_exactly(string name, string logoFile)
+    {
+        DataImporterService.IsFileLogoForTuitionPartner(name, logoFile).Should().BeFalse();
+    }
+}
