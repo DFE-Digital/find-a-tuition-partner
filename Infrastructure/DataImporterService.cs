deb0b90e59faba88208c26c8671b6c7363e43206
Ben Arnold <ben@arnoldfamily.me.uk>
2022-10-10 19:46:35 +0100

    NTP-631: Use modern constructs when processing MemoryStream

--- Infrastructure/DataImporterService.cs
+++ Infrastructure/DataImporterService.cs
@@ -167,13 +167,11 @@ public static class StreamExtensions
 {
     public static byte[] ReadAllBytes(this Stream instream)
     {
-        if (instream is MemoryStream)
-            return ((MemoryStream)instream).ToArray();
+        if (instream is MemoryStream stream)
+            return stream.ToArray();
 
-        using (var memoryStream = new MemoryStream())
-        {
-            instream.CopyTo(memoryStream);
-            return memoryStream.ToArray();
-        }
+        using var memoryStream = new MemoryStream();
+        instream.CopyTo(memoryStream);
+        return memoryStream.ToArray();
     }
 }
