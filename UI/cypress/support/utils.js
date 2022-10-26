478b277cc512be1a8d3cf5b5caaf38fa3bdd2c8c
David Winchurch <david.winchurch@iterativeit.com>
2022-10-26 14:33:05 +0100

    Ensure kebab case algorithm used in service code and end to end tests matches

--- UI/cypress/support/utils.js
+++ UI/cypress/support/utils.js
@@ -2,6 +2,7 @@ export const kebabCase = (string) =>
   string
     .replace(/([a-z])([A-Z])/g, "$1-$2")
     .replace(/[\s_]+/g, "-")
+    .replace(/[^a-zA-Z0-9_{}()\-~/]/g, "")
     .toLowerCase();
 
 export const removeWhitespace = (string) => string.trim().replace(/\s/, "");
