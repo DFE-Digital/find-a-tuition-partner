7bf6b8b0f20d9e3b6f0a18b0ef9317c776f656bb
Ashley Ward <421038+ashward@users.noreply.github.com>
2022-12-14 11:32:22 +0000

    NTP-720: Running prettier!

--- UI/cypress/support/step_definitions/typography.js
+++ UI/cypress/support/step_definitions/typography.js
@@ -1,11 +1,12 @@
 import {
-    Given,
-    When,
-    Then,
-    Step,
-  } from "@badeball/cypress-cucumber-preprocessor";
+  Given,
+  When,
+  Then,
+  Step,
+} from "@badeball/cypress-cucumber-preprocessor";
 
-Then('the heading caption is {string}', (expected) => {
-    cy.get('.govuk-caption-l').first()
-        .should(caption => expect(caption.text().trim()).to.equal(expected));
+Then("the heading caption is {string}", (expected) => {
+  cy.get(".govuk-caption-l")
+    .first()
+    .should((caption) => expect(caption.text().trim()).to.equal(expected));
 });
