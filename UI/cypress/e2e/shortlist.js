b16ebd72602012be99a72d516050156b7e0991f6
Ashley Ward <421038+ashward@users.noreply.github.com>
2022-12-14 11:28:47 +0000

    NTP-720: Adding some cypress tests

---
+++ UI/cypress/e2e/shortlist.js
@@ -0,0 +1,67 @@
+import {
+    Given,
+    When,
+    Then,
+    Step,
+} from "@badeball/cypress-cucumber-preprocessor";
+import { kebabCase } from "../support/utils";
+
+When('they add {string} to their shortlist on the results page', (tpName) => {
+    cy.get(`#shortlist-cb-${kebabCase(tpName)}`)
+        .check();
+});
+
+When('they remove {string} from their shortlist on the results page', (tpName) => {
+    cy.get(`#shortlist-cb-${kebabCase(tpName)}`)
+        .uncheck();
+});
+
+Then('{string} is marked as shortlisted on the results page', (tpName) => {
+    cy.get(`#shortlist-cb-${kebabCase(tpName)}`)
+        .should('be.checked');
+});
+
+Then('{string} is not marked as shortlisted on the results page', (tpName) => {
+    cy.get(`#shortlist-cb-${kebabCase(tpName)}`)
+        .should('not.be.checked');
+});
+
+Then('the shortlist shows as having {int} entries on the results page', (numEntries) => {
+    cy.get('#totalShortlistedTuitionPartners')
+        .should(el => expect(parseInt(el.text().trim())).to.equal(numEntries));
+});
+
+When('they choose to view their shortlist from the results page', () => {
+    cy.get('[data-testid="my-shortlisted-tuition-partners-link"]')
+        .click();
+});
+
+Then('{string} is entry {int} on the shortlist page', (tpName, entry) => {
+    cy.get('tbody th').eq(entry - 1)
+        .should('have.text', tpName);
+});
+
+Then('there are {int} entries on the shortlist page', (count) => {
+    cy.get('tbody th')
+        .should('have.length', count);
+});
+
+Then('{string} is entry {int} on the not available list on the shortlist page', (tpName, entry) => {
+    cy.get('[data-testid="unavailable-tps"] li').eq(entry - 1)
+        .should('contain.text', tpName);
+});
+
+Then('entry {int} on the shortlist is the row {string}, {string}, {string}, {string}, {string}', (entry, name, keyStages, subjects, tuitionType, price) => {
+    cy.get('tbody tr').eq(entry - 1)
+        .within(() => {
+            cy.get('th').should(el => expect(el.text().trim()).to.equal(name));
+            cy.get('td').eq(0).should(el => expect(el.text().trim()).to.equal(keyStages));
+            cy.get('td').eq(1).should(el => expect(el.text().trim()).to.equal(subjects));
+            cy.get('td').eq(2).should(el => expect(el.text().trim()).to.equal(tuitionType));
+            cy.get('td').eq(3).should(el => expect(el.text().trim()).to.equal(price));
+        })
+});
+
+When('they choose to view the {string} details from the shortlist', (tpName) => {
+    cy.get('a').contains(tpName).click();
+});
