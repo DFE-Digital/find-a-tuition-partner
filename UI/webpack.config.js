1fe4b073e195c8c73282872b48e68969b065e487
Ashley Ward <421038+ashward@users.noreply.github.com>
2022-12-15 11:24:03 +0000

    NTP-794: Adding babel to make the JS compatible

--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
         test: /\.mjs$/,
         type: "javascript/auto",
       },
