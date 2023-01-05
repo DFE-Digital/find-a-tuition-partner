0b271ed6ce0fde63913cbad08ef9964fc8298ff7
Kola <Kolapo.Abolarinwa@digital.education.gov.uk>
2023-01-05 11:22:36 +0000

    Refactor code: Clean relevant scss files Use partial page for displaying checkbox(es) for shortlist tuition partner(s) functionality

--- UI/Models/ShortlistCheckboxModel.cs
+++ UI/Models/ShortlistCheckboxModel.cs
@@ -7,4 +7,18 @@ public class ShortlistCheckboxModel
     public string? LabelValue { get; set; }
     public string? CheckboxName { get; set; }
     public bool IsShortlisted { get; set; }
+
+    public ShortlistCheckboxModel()
+    {
+
+    }
+    public ShortlistCheckboxModel(string id, string seoUrl, string labelValue,
+        string checkboxName, bool isShortlisted)
+    {
+        Id = id;
+        SeoUrl = seoUrl;
+        LabelValue = labelValue;
+        CheckboxName = checkboxName;
+        IsShortlisted = isShortlisted;
+    }
 }
