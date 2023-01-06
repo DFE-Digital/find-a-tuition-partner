9e688b853e1b00c746c1bf66efd77eb0d3063682
Ashley Ward <421038+ashward@users.noreply.github.com>
2023-01-06 16:52:40 +0000

    NTP-771: Initial WIP commit to add DfeAnalytics package

---
+++ UI/Analytics/AnalyticsConfigurerService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Dfe.Analytics.AspNetCore;
+
+namespace UI.Analytics
+{
+    internal interface IAnalyticsConfigurerService
+    {
+        void ConfigureApp(IApplicationBuilder app);
+    }
+
+    internal class AnalyticsConfigurerService : IAnalyticsConfigurerService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
+        private readonly bool _isConfigured;
+
+        public AnalyticsConfigurerService(IConfiguration configuration, ILogger<AnalyticsConfigurerService> logger)
+        {
+            this._configuration = configuration;
+            this._logger = logger;
+            this._isConfigured = IsAnalyticsConfigured();
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDfeAnalytics(ConfigureDfeAnalyticsOptions);
+        }
+
+        public void ConfigureApp(IApplicationBuilder app)
+        {
+            if (this.IsAnalyticsConfigured())
+            {
+                app.UseDfeAnalytics();
+            }
+            else
+            {
+                _logger.LogWarning("DfeAnalytics is NOT configured and will be disabled");
+            }
+        }
+
+        private bool IsAnalyticsConfigured()
+        {
+            var section = _configuration.GetSection("DfeAnalytics");
+
+            return (!string.IsNullOrEmpty(section["CredentialsJson"]));
+        }
+
+        private static void ConfigureDfeAnalyticsOptions(DfeAnalyticsOptions options)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Unknown";
+
+            options.Environment = environmentName;
+            options.DatasetId = $"fatp_events_{environmentName.ToLower()}";
+        }
+    }
+}
+
---
+++ UI/Analytics/AnalyticsExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Dfe.Analytics.AspNetCore;
+
+namespace UI.Analytics
+{
+    public static class AnalyticsExtensions
+    {
+        public static IServiceCollection AddAnalytics(this IServiceCollection services)
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            AnalyticsConfigurerService.ConfigureServices(services);
+
+            services.Add(new ServiceDescriptor(typeof(IAnalyticsConfigurerService), typeof(AnalyticsConfigurerService), ServiceLifetime.Singleton));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseAnalytics(this IApplicationBuilder app)
+        {
+            ArgumentNullException.ThrowIfNull(app);
+
+            app.ApplicationServices.GetService<IAnalyticsConfigurerService>()?.ConfigureApp(app);
+
+            return app;
+        }
+    }
+}
+
