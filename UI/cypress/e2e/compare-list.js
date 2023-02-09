832e1c2a6173727dd9a918cecb89c2efe9e43275
Kieran O'Hanlon <k_o_hanlon@yahoo.co.uk>
2023-02-09 15:11:22 +0000

    NTP-874: Cypress test update 2

--- UI/cypress/e2e/compare-list.js
+++ UI/cypress/e2e/compare-list.js
@@ -43,7 +43,6 @@ Then(
 Then(
   "the price comparison list shows as having {int} entries on the results page",
   (numEntries) => {
-    Step(this, "the heading is 'Compare tuition partner prices'");
     cy.get("#totalCompareListedTuitionPartners").should((el) =>
       expect(parseInt(el.text().trim())).to.equal(numEntries)
     );
@@ -73,7 +72,7 @@ Then(
 );
 
 Then("there are {int} entries on the price comparison list page", (count) => {
-  cy.get("tbody th").should("have.length", count);
+  Step(this, "the heading is 'Compare tuition partner prices'");
   cy.get("tbody th").should("have.length", count);
 });
 
