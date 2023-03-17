d0ec9a258c1f596f27355ddf289c04a8f63a2478
Kieran O'Hanlon <k_o_hanlon@yahoo.co.uk>
2023-03-17 18:15:59 +0000

    NTP-1039: Missed files from previous commit

--- Application/Common/Interfaces/Repositories/ITuitionPartnerEnquiryRepository.cs
+++ Application/Common/Interfaces/Repositories/ITuitionPartnerEnquiryRepository.cs
@@ -6,4 +6,5 @@ namespace Application.Common.Interfaces.Repositories;
 public interface ITuitionPartnerEnquiryRepository : IGenericRepository<TuitionPartnerEnquiry>
 {
     Task<EnquirerViewResponseModel?> GetEnquirerViewResponse(int enquiryId, int tuitionPartnerId);
+    Task<EnquirerViewTutionPartnerDetailsModel?> GetEnquirerViewTutionPartnerDetailsResponse(int enquiryId, int tuitionPartnerId);
 }
---
+++ Application/Queries/GetEnquirerViewTutionPartnerDetailsQuery.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models.Enquiry.Respond;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Queries;
+
+public record GetEnquirerViewTutionPartnerDetailsQuery(int EnquiryId, int TuitionPartnerId) : IRequest<EnquirerViewTutionPartnerDetailsModel?>;
+
+public class GetEnquirerViewTutionPartnerDetailsQueryHandler : IRequestHandler<GetEnquirerViewTutionPartnerDetailsQuery, EnquirerViewTutionPartnerDetailsModel?>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    private readonly ILogger<GetEnquirerViewTutionPartnerDetailsQueryHandler> _logger;
+
+    public GetEnquirerViewTutionPartnerDetailsQueryHandler(IUnitOfWork unitOfWork, ILogger<GetEnquirerViewTutionPartnerDetailsQueryHandler> logger)
+    {
+        _unitOfWork = unitOfWork;
+        _logger = logger;
+    }
+
+    public async Task<EnquirerViewTutionPartnerDetailsModel?> Handle(GetEnquirerViewTutionPartnerDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _unitOfWork.TuitionPartnerEnquiryRepository
+            .GetEnquirerViewTutionPartnerDetailsResponse(request.EnquiryId, request.TuitionPartnerId);
+
+        if (result == null)
+        {
+            _logger.LogWarning("Enquiry response TP details not found for the given enquiryId: {enquiryId} and tuitionPartnerId: {tuitionPartnerId}",
+                request.EnquiryId, request.TuitionPartnerId);
+        }
+        return result;
+    }
+}
