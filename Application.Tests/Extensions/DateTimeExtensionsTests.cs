a3af4acb146b847af8d5d318f5364a5f399a36b6
Hasan Ahmed <hasan@hasanme.com>
2023-03-28 18:56:12 +0100

    NTP-1124: Fix an issue with the unit test when BST is longer applicable.

--- Application.Tests/Extensions/DateTimeExtensionsTests.cs
+++ Application.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -14,6 +14,13 @@ public class DateTimeExtensionsTests
         var utcTime = new DateTime(2023, 03, 28, 10, 0, 0); // 10:00 AM UTC
         var expectedUkTime = new DateTime(2023, 03, 28, 11, 0, 0); // 11:00 AM UK local time
 
+        var offset = DateTimeOffset.Now;
+
+        if (offset.Offset == TimeSpan.Zero)
+        {
+            expectedUkTime = utcTime;
+        }
+
         // Act
         var actualUkTime = utcTime.ToLocalDateTime("GMT Standard Time");
 
