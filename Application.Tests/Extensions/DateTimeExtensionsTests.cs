e334d2490e9c894031c79cbc510b3b4a55bcc4fe
Kieran O'Hanlon <k_o_hanlon@yahoo.co.uk>
2023-03-29 13:02:38 +0100

    NTP-826 v2: Merge issues

--- Application.Tests/Extensions/DateTimeExtensionsTests.cs
+++ Application.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -6,26 +6,19 @@ namespace Application.Tests.Extensions;
 
 public class DateTimeExtensionsTests
 {
+    [Fact]
+    public void ToLocalDateTime_ShouldConvertToCorrectTimeZone_Given_UtcDateTime()
+    {
+        // Arrange
+        var dateTimeUtc = new DateTime(2023, 3, 29, 12, 0, 0, DateTimeKind.Utc);
+        var timeZoneId = "GMT Standard Time";
+        var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+        var expectedDateTime = TimeZoneInfo.ConvertTimeFromUtc(dateTimeUtc, timeZoneInfo);
 
-    // Temporarily disabled as it fails on the build server
-    // [Fact]
-    // public void ConvertUtcToUk_LocalDateTimeReturned()
-    // {
-    //     // Arrange
-    //     var utcTime = new DateTime(2023, 03, 28, 10, 0, 0); // 10:00 AM UTC
-    //     var expectedUkTime = new DateTime(2023, 03, 28, 11, 0, 0); // 11:00 AM UK local time
-    //     
-    //     var offset = DateTimeOffset.Now;
-    //     
-    //     if (offset.Offset == TimeSpan.Zero)
-    //     {
-    //         expectedUkTime = utcTime;
-    //     }
-    //
-    //     // Act
-    //     var actualUkTime = utcTime.ToLocalDateTime("GMT Standard Time");
-    //
-    //     // Assert
-    //     Assert.Equal(expectedUkTime, actualUkTime);
-    // }
+        // Act
+        var result = dateTimeUtc.ToLocalDateTime(timeZoneId);
+
+        // Assert
+        Assert.Equal(expectedDateTime, result);
+    }
 }
