2d2c92ce89c4d620aaa5d2c7c9b83ecdbf8c6c5b
kieranohanlon <113545700+kieranohanlon@users.noreply.github.com>
2023-03-29 09:32:25 +0100

    NTP-826: Invalid referer header causes exception to be logged (#307)
    * NTP-826: supress error if invalid referer host
    
    * NTP-1137: Minor search page content updates
    
    * NTP-826: dotnet format

--- UI/Extensions/HttpRequestExtensions.cs
+++ UI/Extensions/HttpRequestExtensions.cs
@@ -11,6 +11,61 @@ namespace UI.Extensions
             }
             return $"{request.Scheme}://{request.Host}{request.PathBase}";
         }
+
+        public static string GetReferer(this HttpRequest request)
+        {
+            var referrer = string.Empty;
+
+            try
+            {
+                referrer = request.Headers["Referer"].ToString();
+            }
+            catch { } //Suppress exception
+
+            return referrer;
+        }
+
+        public static string GetNtpReferer(this HttpRequest request)
+        {
+            var ntpReferer = string.Empty;
+            var referrerHost = GetRefererHost(request);
+
+            if (!string.IsNullOrEmpty(referrerHost))
+            {
+                try
+                {
+                    if (string.Equals(referrerHost, request.Host.Host.ToString(), StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        ntpReferer = GetReferer(request);
+                    }
+                }
+                catch { } //Suppress exception
+            }
+
+            return ntpReferer;
+        }
+
+        public static string GetRefererHost(this HttpRequest request)
+        {
+            var referrerHost = string.Empty;
+            var referrer = GetReferer(request);
+
+            if (!string.IsNullOrEmpty(referrer))
+            {
+                try
+                {
+                    var referrerUri = new Uri(referrer);
+                    referrerHost = referrerUri.Host;
+                }
+                catch
+                {
+                    //Suppress exception
+                    referrerHost = "Invalid Uri";
+                }
+            }
+
+            return referrerHost;
+        }
     }
 }
 
