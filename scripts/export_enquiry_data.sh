ba27543a0f5992b3b4812ab35b0cf07a113a4a96
Ashley Ward <421038+ashward@users.noreply.github.com>
2023-04-18 20:09:12 +0100

    NTP-1186: Adding a script to export the enquiry data to CSVs

---
+++ scripts/export_enquiry_data.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+ENVIRONMENT=${1:-"production"}
+CF_SPACE="ntp-$ENVIRONMENT"
+DB_SERVICE="find-a-tuition-partner-$ENVIRONMENT-postgres-db"
+
+DATE_DIR=$(date +%Y-%m-%d_%H%M%S)
+EXPORT_DIR="./exports/$ENVIRONMENT/$DATE_DIR"
+
+mkdir -p $EXPORT_DIR
+
+cf target -s $CF_SPACE
+
+cf conduit $DB_SERVICE -c '{"read_only": true}' -- psql -c '\COPY ( 
+        SELECT "Enquiries"."SupportReferenceNumber" AS "Reference",
+        "TutoringLogistics",
+        "SENDRequirements",
+        "AdditionalInformation",
+        "TuitionTypes"."Name" AS "TuitionType",
+        "CreatedAt"
+        FROM "Enquiries" LEFT JOIN "TuitionTypes" ON "Enquiries"."TuitionTypeId" = "TuitionTypes"."Id"
+        ORDER BY "CreatedAt" ASC 
+    )
+    TO '\'$EXPORT_DIR'/enquiries.csv'\''
+    WITH ( FORMAT CSV, HEADER );'
+
+cf conduit $DB_SERVICE -c '{"read_only": true}' -- psql -c '\COPY ( 
+    SELECT "Enquiries"."SupportReferenceNumber" AS "Reference",
+        "TuitionPartners"."Name" AS "TuitionPartner",
+        "EnquiryResponses"."KeyStageAndSubjectsText" AS "KeyStageAndSubjects",
+        "EnquiryResponses"."TuitionTypeText" AS "TuitionType",
+        "EnquiryResponses"."TutoringLogisticsText" AS "TutoringLogistics",
+        "EnquiryResponses"."SENDRequirementsText" AS "SENDRequirements",
+        "EnquiryResponses"."AdditionalInformationText" AS "AdditionalInformation",
+        "EnquiryResponses"."CompletedAt"
+        FROM "Enquiries"
+        INNER JOIN "TuitionPartnersEnquiry" ON "Enquiries"."Id" = "TuitionPartnersEnquiry"."EnquiryId"
+        INNER JOIN "TuitionPartners" ON "TuitionPartnersEnquiry"."TuitionPartnerId" = "TuitionPartners"."Id"
+        INNER JOIN "EnquiryResponses" ON "TuitionPartnersEnquiry"."EnquiryResponseId" = "EnquiryResponses"."Id"
+        ORDER BY "Enquiries"."CreatedAt" ASC, "EnquiryResponses"."CompletedAt" ASC
+    )
+    TO '\'$EXPORT_DIR'/responses.csv'\''
+    WITH ( FORMAT CSV, HEADER );'
