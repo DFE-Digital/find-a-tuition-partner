479ebfc307db1727b5a1ec3c89d182f5615c94dd
Ashley Ward <421038+ashward@users.noreply.github.com>
2023-04-19 09:41:43 +0100

    Combining into a single `psql` command

--- scripts/export_enquiry_data.sh
+++ scripts/export_enquiry_data.sh
@@ -10,33 +10,34 @@ mkdir -p $EXPORT_DIR
 
 cf target -s $CF_SPACE
 
-cf conduit $DB_SERVICE -c '{"read_only": true}' -- psql -c '\COPY ( 
+cf conduit $DB_SERVICE -c '{"read_only": true}' -- psql \
+    -c '\COPY ( 
+            SELECT "Enquiries"."SupportReferenceNumber" AS "Reference",
+            "TutoringLogistics",
+            "SENDRequirements",
+            "AdditionalInformation",
+            "TuitionTypes"."Name" AS "TuitionType",
+            "CreatedAt"
+            FROM "Enquiries" LEFT JOIN "TuitionTypes" ON "Enquiries"."TuitionTypeId" = "TuitionTypes"."Id"
+            ORDER BY "CreatedAt" ASC 
+        )
+        TO '\'$EXPORT_DIR'/enquiries.csv'\''
+        WITH ( FORMAT CSV, HEADER );' \
+    \
+    -c '\COPY ( 
         SELECT "Enquiries"."SupportReferenceNumber" AS "Reference",
-        "TutoringLogistics",
-        "SENDRequirements",
-        "AdditionalInformation",
-        "TuitionTypes"."Name" AS "TuitionType",
-        "CreatedAt"
-        FROM "Enquiries" LEFT JOIN "TuitionTypes" ON "Enquiries"."TuitionTypeId" = "TuitionTypes"."Id"
-        ORDER BY "CreatedAt" ASC 
-    )
-    TO '\'$EXPORT_DIR'/enquiries.csv'\''
-    WITH ( FORMAT CSV, HEADER );'
-
-cf conduit $DB_SERVICE -c '{"read_only": true}' -- psql -c '\COPY ( 
-    SELECT "Enquiries"."SupportReferenceNumber" AS "Reference",
-        "TuitionPartners"."Name" AS "TuitionPartner",
-        "EnquiryResponses"."KeyStageAndSubjectsText" AS "KeyStageAndSubjects",
-        "EnquiryResponses"."TuitionTypeText" AS "TuitionType",
-        "EnquiryResponses"."TutoringLogisticsText" AS "TutoringLogistics",
-        "EnquiryResponses"."SENDRequirementsText" AS "SENDRequirements",
-        "EnquiryResponses"."AdditionalInformationText" AS "AdditionalInformation",
-        "EnquiryResponses"."CompletedAt"
-        FROM "Enquiries"
-        INNER JOIN "TuitionPartnersEnquiry" ON "Enquiries"."Id" = "TuitionPartnersEnquiry"."EnquiryId"
-        INNER JOIN "TuitionPartners" ON "TuitionPartnersEnquiry"."TuitionPartnerId" = "TuitionPartners"."Id"
-        INNER JOIN "EnquiryResponses" ON "TuitionPartnersEnquiry"."EnquiryResponseId" = "EnquiryResponses"."Id"
-        ORDER BY "Enquiries"."CreatedAt" ASC, "EnquiryResponses"."CompletedAt" ASC
-    )
-    TO '\'$EXPORT_DIR'/responses.csv'\''
-    WITH ( FORMAT CSV, HEADER );'
+            "TuitionPartners"."Name" AS "TuitionPartner",
+            "EnquiryResponses"."KeyStageAndSubjectsText" AS "KeyStageAndSubjects",
+            "EnquiryResponses"."TuitionTypeText" AS "TuitionType",
+            "EnquiryResponses"."TutoringLogisticsText" AS "TutoringLogistics",
+            "EnquiryResponses"."SENDRequirementsText" AS "SENDRequirements",
+            "EnquiryResponses"."AdditionalInformationText" AS "AdditionalInformation",
+            "EnquiryResponses"."CompletedAt"
+            FROM "Enquiries"
+            INNER JOIN "TuitionPartnersEnquiry" ON "Enquiries"."Id" = "TuitionPartnersEnquiry"."EnquiryId"
+            INNER JOIN "TuitionPartners" ON "TuitionPartnersEnquiry"."TuitionPartnerId" = "TuitionPartners"."Id"
+            INNER JOIN "EnquiryResponses" ON "TuitionPartnersEnquiry"."EnquiryResponseId" = "EnquiryResponses"."Id"
+            ORDER BY "Enquiries"."CreatedAt" ASC, "EnquiryResponses"."CompletedAt" ASC
+        )
+        TO '\'$EXPORT_DIR'/responses.csv'\''
+        WITH ( FORMAT CSV, HEADER );'
