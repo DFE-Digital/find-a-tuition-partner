a43610afa06a11de69367994a1b4a0bad8a641e1
kieranohanlon <k_o_hanlon@yahoo.co.uk>
2023-05-31 15:46:23 +0100

    NTP-1151: Add in polling function for emails

---
+++ Functions/PollEmailProcessing.cs
@@ -0,0 +1,60 @@
+using System.Text.RegularExpressions;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Functions
+{
+    public class PollEmailProcessing
+    {
+        private readonly ILogger _logger;
+
+        public PollEmailProcessing(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<PollEmailProcessing>();
+        }
+
+        [Function("PollEmailProcessing")]
+        public async Task RunAysnc([TimerTrigger("* * * * *")] MyInfo myTimer)
+        {
+            var url = Environment.GetEnvironmentVariable("PollEmailProcessingUrl", EnvironmentVariableTarget.Process);
+            url ??= "https://find-a-tuition-partner-qa.london.cloudapps.digital/admin/process-emails";
+
+            _logger.LogInformation($"Find a Tuition Partner PollEmailProcessing function executed at: {DateTime.UtcNow}.  Polling {url}");
+
+            var httpClient = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true });
+
+            var httpResponseMessage = await httpClient.GetAsync(url);
+
+            var content = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            var regex = new Regex(@"(?s)process-emails-outcome"">(.*?)</p>", RegexOptions.Multiline);
+
+            var match = regex.Match(content);
+
+            if (match.Success)
+            {
+                _logger.LogInformation(match.Groups[1].Value);
+            }
+            else
+            {
+                _logger.LogInformation($"Output Id not found, full content... {content}");
+            }
+        }
+    }
+
+    public class MyInfo
+    {
+        public MyScheduleStatus? ScheduleStatus { get; set; }
+
+        public bool IsPastDue { get; set; }
+    }
+
+    public class MyScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
