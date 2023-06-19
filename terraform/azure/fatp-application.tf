cfb684c8e2fbeecf15982163e12a01c6caeff210
Hasan Ahmed <hasan@hasanme.com>
2023-06-19 16:56:20 +0100

    NTP-880: Add E2E cypress test.
    - Get the We app name, resource group and Front door host name from the terraform output
    
    - Also bumped up the version of the web app services hosting module

--- terraform/azure/fatp-application.tf
+++ terraform/azure/fatp-application.tf
@@ -1,5 +1,5 @@
 module "fatp_azure_web_app_services_hosting" {
-  source = "github.com/hasan3d/terraform-azurerm-web-app-services-hosting?ref=v0.1.5"
+  source = "github.com/hasan3d/terraform-azurerm-web-app-services-hosting?ref=v0.1.6"
 
   environment    = local.environment
   project_name   = local.project_name
