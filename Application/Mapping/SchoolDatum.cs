3282f57533fc175000339014a992ab67969fe96e
kieranohanlon <k_o_hanlon@yahoo.co.uk>
2023-06-22 11:43:58 +0100

    NTP-1243: Re-seed LA data and exclude 'offshore schools' from GIAS processing

--- Application/Mapping/SchoolDatum.cs
+++ Application/Mapping/SchoolDatum.cs
@@ -29,7 +29,9 @@ public class SchoolDatum
         if (EstablishmentTypeGroup == (int)EstablishmentTypeGroups.WelshSchools)
             return false;
 
-        if (EstablishmentType == EstablishmentTypes.Id.BritishSchoolsOverseas || EstablishmentType == EstablishmentTypes.Id.ServiceChildrensEducation)
+        if (EstablishmentType == EstablishmentTypes.Id.OffshoreSchools || 
+            EstablishmentType == EstablishmentTypes.Id.BritishSchoolsOverseas || 
+            EstablishmentType == EstablishmentTypes.Id.ServiceChildrensEducation)
             return false;
 
         if (!EstablishmentNumber.HasValue)
