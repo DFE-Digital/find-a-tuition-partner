916c2083b440e38a4a761d88bb3814a2eea1f68f
Hasan Ahmed <hasan@hasanme.com>
2023-07-31 19:20:09 +0100

    NTP-880: Remove the step to print the value for the e2e test variable value and correctly check for e2e test if condition
    - Increase the cdn (front door) latency alert threshold from 1 second to two second.

--- terraform/azure/modules/fatp_azure_web_app_services_hosting/monitor.tf
+++ terraform/azure/modules/fatp_azure_web_app_services_hosting/monitor.tf
@@ -179,7 +179,7 @@ resource "azurerm_monitor_metric_alert" "latency" {
   name                = "${azurerm_cdn_frontdoor_profile.cdn[0].name}-latency"
   resource_group_name = local.resource_group.name
   scopes              = [azurerm_cdn_frontdoor_profile.cdn[0].id]
-  description         = "Action will be triggered when Origin latency is higher than 1s"
+  description         = "Action will be triggered when Origin latency is higher than 2s"
   window_size         = "PT5M"
   frequency           = "PT5M"
   severity            = 2
@@ -189,7 +189,7 @@ resource "azurerm_monitor_metric_alert" "latency" {
     metric_name      = "TotalLatency"
     aggregation      = "Average"
     operator         = "GreaterThan"
-    threshold        = 1000
+    threshold        = 2000
   }
 
   action {
