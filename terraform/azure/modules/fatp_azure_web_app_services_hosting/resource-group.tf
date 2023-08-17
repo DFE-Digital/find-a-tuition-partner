44c6c9d857fe957ead9534a135aaa9deb0257832
Hasan Ahmed <hasan@hasanme.com>
2023-08-17 12:35:14 +0100

    NTP-1288: Creating Scoped Lock on resource group failed due to lack of permission therefore removing that logic.

--- terraform/azure/modules/fatp_azure_web_app_services_hosting/resource-group.tf
+++ terraform/azure/modules/fatp_azure_web_app_services_hosting/resource-group.tf
@@ -5,10 +5,3 @@ resource "azurerm_resource_group" "default" {
   location = local.azure_location
   tags     = local.tags
 }
-
-resource "azurerm_management_lock" "resource_group" {
-  name       = "resource-group-level"
-  scope      = azurerm_resource_group.default[0].id
-  lock_level = "CanNotDelete"
-  notes      = "The resource group is not allowed to delete"
-}
